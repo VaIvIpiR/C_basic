872238ca45686b3d6c4e135aceb855ddca076ad5
VaIvIpiR <vaivipir@ukr.net>
2019-03-25 01:04:15 +0200

    Десь не виходжу з циклу.

--- Fibonacci_number/Fibonacci_number/Program.cs
+++ Fibonacci_number/Fibonacci_number/Program.cs
@@ -9,11 +9,15 @@ namespace Fibonacci_number
     class Program
     {
         private static Int32 a;
+        private static Int32 n;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Fibonacci number.");
             Console.Write("Enter position: ");
             Tryparse_a();
+            Fibonacci_position(n);
+            Console.ReadKey();
         }
 
         static void Tryparse_a()
@@ -35,8 +39,10 @@ namespace Fibonacci_number
 
         static int Fibonacci_position(int n)
         {
-            return Fibonacci_position(n - 1) + Fibonacci_position(n - 2);
-
+            if (n == 0 || n == 1)
+                return 1;
+          else return Fibonacci_position(n - 1) + Fibonacci_position(n - 2);          
+          
         }
     }
 }
