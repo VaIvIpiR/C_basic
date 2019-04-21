7b6510a44a3aba9c74eaafc6771c1149d1a229f4
VaIvIpiR <vaivipir@ukr.net>
2019-04-21 19:31:15 +0300

    Simple classes

---
+++ Homework/Homework/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Phone { get; private set; }
+        public string Address { get; private set; }
+        public string Email { get; private set; }
+
+        private DateTime dateOfBirth;
+        public string Age { get; private set; }
+
+        public string GetAge()
+        {
+            System.DateTime dateNow = DateTime.Now;
+            return (dateNow.Year - dateOfBirth.Year).ToString();
+        }
+
+    }
+}
+
